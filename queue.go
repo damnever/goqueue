d102a623a0c294c29e4ed341f93066816944ce74
Damnever <dxc.wolf@gmail.com>
2015-12-09 22:01:19 +0800

    queue

--- queue.go
+++ queue.go
@@ -1,7 +1,5 @@
 /*
-  A thread safe queue written in Go.
-  Golang do not support infinite size channel.
-  It can get element block with timeout.
+  A thread safe queue for Golang.
 */
 package goqueue
 
@@ -12,34 +10,63 @@ import (
 	"time"
 )
 
-type Queue struct {
-	mutex   sync.Mutex
-	getCond *sync.Cond
-	putCond *sync.Cond
-	list    *list.List
+type EmptyQueueError struct{}
+
+type FullQueueError struct{}
+
+func (err *EmptyQueueError) Error() string {
+	return "Queue is Empty"
 }
 
-func NewQueue() *Queue {
+func (err *FullQueueError) Error() string {
+	return "Queue is Full"
+}
+
+type Queue struct {
+	mutex          sync.Mutex
+	getCond        *sync.Cond
+	putCond        *sync.Cond
+	taskDoneCond   *sync.Cond
+	list           *list.List
+	maxSize        int
+	unfinishedTask int
+}
+
+func NewQueue(maxSize int) *Queue {
 	queue := new(Queue)
 	queue.mutex = sync.Mutex{}
 	queue.getCond = sync.NewCond(&queue.mutex)
 	queue.putCond = sync.NewCond(&queue.mutex)
+	queue.taskDoneCond = sync.NewCond(&queue.mutex)
 	queue.list = list.New()
+	queue.maxSize = maxSize
+	queue.unfinishedTask = 0
 	return queue
 }
 
 func (queue *Queue) Size() int {
 	queue.mutex.Lock()
-	size := queue.list.Len()
+	size := queue.qsize()
 	queue.mutex.Unlock()
 	return size
 }
 
 func (queue *Queue) IsEmpty() bool {
 	queue.mutex.Lock()
-	isEmpty := queue.isEmpty()
+	isEmpty := (queue.qsize() == 0)
 	queue.mutex.Unlock()
 	return isEmpty
+}
+
+func (queue *Queue) IsFull() bool {
+	queue.mutex.Lock()
+	isFull := (queue.maxSize > 0 && queue.qsize() == queue.maxSize)
+	queue.mutex.Unlock()
+	return isFull
+}
+
+func (queue *Queue) GetNoWait() (*list.Element, error) {
+	return queue.Get(false, 0)
 }
 
 func (queue *Queue) Get(block bool, timeout float64) (*list.Element, error) {
@@ -47,11 +74,11 @@ func (queue *Queue) Get(block bool, timeout float64) (*list.Element, error) {
 	defer queue.getCond.L.Unlock()
 	emptyQ := false
 	if !block {
-		if queue.isEmpty() {
+		if queue.qsize() == 0 {
 			emptyQ = true
 		}
 	} else if timeout == float64(0) {
-		for queue.isEmpty() {
+		for queue.qsize() == 0 {
 			queue.getCond.Wait()
 		}
 	} else if timeout < float64(0) {
@@ -59,15 +86,15 @@ func (queue *Queue) Get(block bool, timeout float64) (*list.Element, error) {
 	} else {
 		timer := time.After(time.Duration(timeout) * time.Second)
 		notEmpty := make(chan bool)
-		go queue.wait(notEmpty)
-		for queue.isEmpty() {
+		go queue.waitSignal(queue.getCond, notEmpty)
+		for queue.qsize() == 0 {
 			select {
 			case <-timer:
 				emptyQ = true
 				break
 			case <-notEmpty:
-				if queue.isEmpty() {
-					go queue.wait(notEmpty)
+				if queue.qsize() == 0 {
+					go queue.waitSignal(queue.getCond, notEmpty)
 				} else {
 					break
 				}
@@ -75,26 +102,87 @@ func (queue *Queue) Get(block bool, timeout float64) (*list.Element, error) {
 		}
 	}
 	if emptyQ {
-		return &list.Element{}, errors.New("Empty Queue")
+		return &list.Element{}, &EmptyQueueError{}
 	}
 	e := queue.list.Front()
 	queue.list.Remove(e)
+	queue.putCond.Signal()
 	return e, nil
 }
 
-func (queue *Queue) Put(element interface{}) *list.Element {
-	queue.putCond.L.Lock()
-	defer queue.putCond.L.Unlock()
-	e := queue.list.PushBack(element)
-	queue.getCond.Signal()
-	return e
+func (queue *Queue) PutNoWait(element interface{}) (*list.Element, error) {
+	return queue.Put(element, false, 0)
 }
 
-func (queue *Queue) wait(c chan<- bool) {
-	queue.getCond.Wait()
+func (queue *Queue) Put(element interface{}, block bool, timeout float64) (*list.Element, error) {
+	queue.putCond.L.Lock()
+	defer queue.putCond.L.Unlock()
+	fullQ := false
+	if queue.maxSize > 0 {
+		if !block {
+			if queue.qsize() == queue.maxSize {
+				fullQ = true
+			}
+		} else if timeout == float64(0) {
+			for queue.qsize() == queue.maxSize {
+				queue.putCond.Wait()
+			}
+		} else if timeout < float64(0) {
+			return &list.Element{}, errors.New("'timeout' must be a non-negative number")
+		} else {
+			timer := time.After(time.Duration(timeout) * time.Second)
+			notFull := make(chan bool)
+			go queue.waitSignal(queue.putCond, notFull)
+			for queue.qsize() == queue.maxSize {
+				select {
+				case <-timer:
+					fullQ = true
+					break
+				case <-notFull:
+					if queue.qsize() == queue.maxSize {
+						go queue.waitSignal(queue.putCond, notFull)
+					} else {
+						break
+					}
+				}
+			}
+		}
+	}
+	if fullQ {
+		return &list.Element{}, &FullQueueError{}
+	}
+	e := queue.list.PushBack(element)
+	queue.unfinishedTask += 1
+	queue.getCond.Signal()
+	return e, nil
+}
+
+func (queue *Queue) TaskDone() {
+	queue.taskDoneCond.L.Lock()
+	defer queue.taskDoneCond.L.Unlock()
+	unfinished := queue.unfinishedTask - 1
+	if unfinished <= 0 {
+		if unfinished < 0 {
+			panic(errors.New("TaskDone() called too many times"))
+		}
+		queue.taskDoneCond.Broadcast()
+	}
+	queue.unfinishedTask = unfinished
+}
+
+func (queue *Queue) WaitAllComplete() {
+	queue.taskDoneCond.L.Lock()
+	defer queue.taskDoneCond.L.Unlock()
+	for queue.unfinishedTask != 0 {
+		queue.taskDoneCond.Wait()
+	}
+}
+
+func (queue *Queue) waitSignal(cond *sync.Cond, c chan<- bool) {
+	cond.Wait()
 	c <- true
 }
 
-func (queue *Queue) isEmpty() bool {
-	return (queue.list.Len() == 0)
+func (queue *Queue) qsize() int {
+	return queue.list.Len()
 }
--- queue_test.go
+++ queue_test.go
@@ -2,17 +2,19 @@ package goqueue
 
 import (
 	"fmt"
+	"reflect"
+	"sync"
 	"testing"
 	"time"
 )
 
-func TestUnBlockGet(t *testing.T) {
+func TestUnBlockGetPut(t *testing.T) {
 	value := 8888
 
-	fmt.Println("Test unblock get, should no error...")
-	queue := NewQueue()
-	queue.Put(value)
-	e, err := queue.Get(false, 0)
+	fmt.Println("Test unblock get/put, should no error...")
+	queue := NewQueue(1)
+	queue.PutNoWait(value)
+	e, err := queue.GetNoWait()
 	if err != nil {
 		t.Fatalf("Unexpect error: %v\n", err)
 	} else if e.Value.(int) != value {
@@ -20,9 +22,9 @@ func TestUnBlockGet(t *testing.T) {
 	}
 	fmt.Println("  ...PASSED")
 
-	fmt.Println("Test unblock get, error should show up...")
-	emptyQ := NewQueue()
-	_, err1 := emptyQ.Get(false, 0)
+	fmt.Println("Test unblock get/put, error should show up...")
+	emptyQ := NewQueue(1)
+	_, err1 := emptyQ.GetNoWait()
 	if err1 == nil {
 		t.Fatalf("No error show up\n")
 	}
@@ -30,10 +32,11 @@ func TestUnBlockGet(t *testing.T) {
 }
 
 func TestBlockGetWithTimeout(t *testing.T) {
-	w := make(chan bool)
-	queue := NewQueue()
+	wg := &sync.WaitGroup{}
+	queue := NewQueue(1)
 
 	get := func(timeout float64, value int, noErr bool) {
+		defer wg.Done()
 		e, err := queue.Get(true, timeout)
 		if noErr {
 			if err == nil && e.Value.(int) != value {
@@ -41,26 +44,169 @@ func TestBlockGetWithTimeout(t *testing.T) {
 			} else if err != nil {
 				t.Fatalf("Unexpect error: %v\n", err)
 			}
-		} else if err == nil {
-			t.Fatalf("Wanted an error, but got nil\n")
+		} else {
+			if err == nil {
+				t.Fatalf("Wanted EmptyQueueError, but got nil\n")
+			} else if reflect.TypeOf(err) != reflect.TypeOf(&EmptyQueueError{}) {
+				t.Fatalf("Expect %v, got %v\n", EmptyQueueError{}, err)
+			}
 		}
-		w <- true
 	}
 
 	put := func(d time.Duration, value int) {
 		time.Sleep(time.Second * d)
-		queue.Put(value)
+		queue.PutNoWait(value)
 	}
 
-	fmt.Println("Test block get with timeout, should no error...")
-	go put(time.Duration(2), 8888)
-	go get(3, 8888, true)
-	<-w
+	fmt.Println("Test block get without timeout, wait utill an element available...")
+	wg.Add(1)
+	go put(time.Duration(3), 9999)
+	go get(0, 9999, true)
+	wg.Wait()
 	fmt.Println("  ...PASSED")
 
-	fmt.Println("Test block get with timeout, error should show up...")
-	go put(time.Duration(4), 8888)
-	go get(3, 8888, false)
-	<-w
+	fmt.Println("Test block get without timeout, wait forever...")
+	done := make(chan bool)
+	queue2 := NewQueue(1)
+	go func() {
+		queue2.Get(true, 0)
+		done <- true
+	}()
+	select {
+	case <-done:
+		t.Fatalf("Queue.Get returned even though no element in Queue\n")
+	case <-time.After(time.Duration(2) * time.Second):
+	}
+	fmt.Println("  ...PASSED")
+	queue2.PutNoWait(0)
+	<-done
+	close(done)
+
+	fmt.Println("Test block get with timeout, if an element available before timeout, return immediately...")
+	wg.Add(1)
+	go put(time.Duration(2), 8888)
+	go get(3, 8888, true)
+	wg.Wait()
+	fmt.Println("  ...PASSED")
+
+	fmt.Println("Test block get with timeout, if no element is available within timeout, return EmptyQueueError...")
+	wg.Add(1)
+	go put(time.Duration(4), 7777)
+	go get(2, 7777, false)
+	wg.Wait()
+	fmt.Println("  ...PASSED")
+}
+
+func TestBlockPutWithTimeout(t *testing.T) {
+	queue := NewQueue(1)
+	queue.PutNoWait(1111)
+	wg := &sync.WaitGroup{}
+
+	get := func(d time.Duration) {
+		time.Sleep(time.Second * d)
+		queue.GetNoWait()
+	}
+
+	put := func(timeout float64, value int, noErr bool) {
+		defer wg.Done()
+		e, err := queue.Put(value, true, timeout)
+		if noErr {
+			if err == nil && e.Value.(int) != value {
+				t.Fatalf("Expect %v, got %v\n", value, e.Value.(int))
+			} else if err != nil {
+				t.Fatalf("Unexpect error: %v\n", err)
+			}
+		} else {
+			if err == nil {
+				t.Fatalf("Wanted FullQueueError, but got nil\n")
+			} else if reflect.TypeOf(err) != reflect.TypeOf(&FullQueueError{}) {
+				t.Fatalf("Expect %v, got %v\n", FullQueueError{}, err)
+			}
+		}
+	}
+
+	fmt.Println("Test block put without timeout, wait utill a free slot available...")
+	go get(time.Duration(2))
+	wg.Add(1)
+	go put(0, 9999, true)
+	wg.Wait()
+	fmt.Println("  ...PASSED")
+
+	fmt.Println("Test block put without timeout, wait forever...")
+	queue2 := NewQueue(1)
+	queue2.PutNoWait(1111)
+	done := make(chan bool)
+	go func() {
+		queue2.Put(0, true, 2222)
+		done <- true
+	}()
+	select {
+	case <-done:
+		t.Fatalf("Queue.Put returned even though no free slot in Queue\n")
+	case <-time.After(time.Duration(2) * time.Second):
+	}
+	fmt.Println("  ...PASSED")
+	queue2.GetNoWait()
+	<-done
+
+	fmt.Println("Test block put with timeout, if a free slot is available before timeout, return immediately...")
+	go get(time.Duration(2))
+	wg.Add(1)
+	go put(3, 8888, true)
+	wg.Wait()
+	fmt.Println("  ...PASSED")
+
+	fmt.Println("Test block put with timeout, if no free slot is available within timeout, return FullQueueError...")
+	go get(time.Duration(4))
+	wg.Add(1)
+	go put(2, 7777, false)
+	wg.Wait()
+	fmt.Println("  ...PASSED")
+}
+
+func TestTaskDoneAndFIFO(t *testing.T) {
+	size := 3
+	queue := NewQueue(size)
+
+	fmt.Println("Test Queue.TaskDone method and FIFO...")
+	for i := 0; i < size; i++ {
+		queue.PutNoWait(i)
+	}
+	for i := 0; i < size; i++ {
+		e, err := queue.GetNoWait()
+		if err != nil {
+			t.Fatalf("Unexpect error: %v\n", err)
+		} else if e.Value.(int) != i {
+			t.Fatalf("Queue is not FIFO")
+		}
+		queue.TaskDone()
+	}
+	if queue.Size() != 0 {
+		t.Fatalf("Something wrong, %v task(s) not done\n", queue.Size())
+	}
+	fmt.Println("  ...PASSED")
+}
+
+func TestWaitAllComplete(t *testing.T) {
+	size := 20
+	queue := NewQueue(size)
+
+	fmt.Println("Test Queue.WaitAllComplete method...")
+	go func() {
+		for !queue.IsEmpty() {
+			_, err := queue.Get(true, 0)
+			if err != nil {
+				t.Fatalf("Unexpect Error: %v\n", err)
+			}
+			queue.TaskDone()
+		}
+	}()
+	for i := 0; i < size; i++ {
+		queue.PutNoWait(i)
+	}
+	queue.WaitAllComplete()
+	if queue.Size() != 0 {
+		t.Fatalf("Somthing wrong, %v task(s) not done\n", queue.Size())
+	}
 	fmt.Println("  ...PASSED")
 }
